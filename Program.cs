d3ab19fd5cbe38ff838aa30f76b68a6988b77bfc
vusalnasirli <7bgzt9j@code.edu.az>
2022-03-15 08:37:43 +0400

    task2

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region
+            //int num1;
+
+            //Console.WriteLine("Accept number:");
+            //num1 = Convert.ToInt32(Console.ReadLine());
+            //if (num1 == 0 || num1 == 1)
+            //{
+            //    Console.WriteLine(num1 + " is not prime number");
+            //    Console.ReadLine();
+            //}
+            //else
+            //{
+            //    for (int a = 2; a <= num1 / 2; a++)
+            //    {
+            //        if (num1 % a == 0)
+            //        {
+            //            Console.WriteLine(num1 + " is not prime number");
+            //            return;
+            //        }
+
+            //    }
+            //    Console.WriteLine(num1 + " is a prime number");
+            //    Console.ReadLine();
+            #region
+            {
+                //int number = 0, count = 0;
+                //Console.Write("Enter number :");
+                //number = Convert.ToInt32(Console.ReadLine());
+                //while (number > 0)
+                //{
+                //    number = number / 10;
+                //    count++;
+                //}
+                //Console.WriteLine("Number of digits is : " + count);
+                //Console.ReadLine();
+            }
+            #region
+            //int n, r, sum = 0, temp;
+            //Console.Write("Enter the Number: ");
+            //n = int.Parse(Console.ReadLine());
+            //temp = n;
+            //while (n > 0)
+            //{
+            //    r = n % 10;
+            //    sum = (sum * 10) + r;
+            //    n = n / 10;
+            //}
+            //if (temp == sum)
+            //    Console.Write("Number is Palindrome.");
+            //else
+            //    Console.Write("Number is not Palindrome");
+            #endregion
+        }
+        #endregion
+
+        
+
+        #endregion
+    }
+}
